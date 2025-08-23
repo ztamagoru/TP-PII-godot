e0ce7620919f6589ad879a7a7452fbc0e00f8b5a
ztamagoru <zdamaria119@gmail.com>
2025-08-23 10:11:18 -0300

    botón de "nueva partida" cambia de escena al tutorial

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
-func new_game_pressed():
+@export var tutorial : PackedScene
+
+func _ready():
 	pass
+
+func new_game_pressed():
+	get_tree().change_scene_to_packed(tutorial)
 
 func continue_game_pressed():
 	pass
