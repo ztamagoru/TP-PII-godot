4513c72af9019daf658b27b4c2ee126e22514f54
ztamagoru <zdamaria119@gmail.com>
2025-08-26 16:03:25 -0300

    Añadido un menú de ajustes del juego, la opción de cambiar la resolución al estar en modo ventana, y el toggle del modo pantalla completa. El cambio de resolución no se previsualiza si el juego se debugea embebido en el editor.

---
+++ scenes/components/ui/settings_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var resolutions_option_button = $MarginContainer/VBoxContainer/TabContainer/Video/MarginContainer/HBoxContainer/VideoSettings/ResolutionOptionButton
+
+func _ready():
+	add_resolution()
+
+func add_resolution():
+	for r in GUI.resolutions:
+		resolutions_option_button.add_item(r)
+
+func update_button_values():
+	var window_size_string = str(get_window().size.x, "x", get_window().size.y)
+	var resolutions_index = GUI.resolutions.keys().find(window_size_string)
+	
+	resolutions_option_button.selected = resolutions_index
+
+func _on_resolution_option_button_item_selected(index):
+	var key = resolutions_option_button.get_item_text(index)
+	DisplayServer.window_set_size(GUI.resolutions[key])
+	
+	GUI.center_window()
+
+func _on_fullscreen_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -12,7 +12,12 @@ func continue_game_pressed():
 	pass
 
 func settings_open():
-	pass
+	var settings_menu = get_node("/root/GUI/SettingsMenu")
+	settings_menu.visible = !settings_menu.visible
+	
+	if settings_menu.visible:
+		#print("se debería haber abierto el menú de ajustes")
+		settings_menu.update_button_values()
 
 func quit_pressed():
 	get_tree().quit()
