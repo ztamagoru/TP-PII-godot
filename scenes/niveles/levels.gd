8516b85443a7ca126baa661351e88ea112bcfde3
pazlore <Mariapazlorenzo11@gmail.com>
2025-11-20 11:57:43 -0300

    niveles 2 y 3 arreglado + plataformas

--- scenes/niveles/levels.gd
+++ scenes/niveles/levels.gd
@@ -13,13 +13,17 @@ func _ready():
 			zonas_respawn.append(child)
 	
 	Globales.current_level = self
+	
 
 func _physics_process(_delta):
 	for body in death_zone.get_overlapping_bodies():
 		if body.is_in_group("jugador"):
-			var current_respawn = zonas_respawn[0]
-			Globales.jugador.global_position = current_respawn.global_position
-			Globales.add_death()
+			respawn()
+
+func respawn():
+	var current_respawn = zonas_respawn[0]
+	Globales.jugador.global_position = current_respawn.global_position
+	Globales.add_death()
 
 func get_next_respawn():
 	zonas_respawn.pop_front()
--- scenes/niveles/obstaculo.gd
+++ scenes/niveles/obstaculo.gd
@@ -5,6 +5,13 @@ var distancia: float = 100
 var velocidad: float = 200.0
 var direccion: Vector2 = Vector2.DOWN
 
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.is_in_group("jugador"):
+		get_parent().respawn()
+
 func _physics_process(delta: float) -> void:
 	position += direccion * velocidad * delta
 
